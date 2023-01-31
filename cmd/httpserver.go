7668a2b6625907c2ce6086233aadbf73a5a51261
Uffy <uffy@users.noreply.github.com>
2023-01-31 10:15:54 +0000

    add a http server

---
+++ cmd/httpserver.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+var httpServerCmd = &cobra.Command{
+	Use:   "server",
+	Short: "run a http server",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		l, err := net.Listen("tcp", ":0")
+		if err != nil {
+			return err
+		}
+		log.Println("listing", l.Addr().String())
+		return http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("hello, world!"))
+		}))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(httpServerCmd)
+}
